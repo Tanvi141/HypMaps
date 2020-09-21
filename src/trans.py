b3f550ab996bf3cd7646c25972a48b09bf0a4b77
Tanvi <tkarandikar@gmail.com>
2020-09-21 21:05:55 +0530

    geodesics

--- src/trans.py
+++ src/trans.py
@@ -9,11 +9,29 @@ class Points():
 		if np.all(self.norm() < 1) == False:
 			raise Exception("Vectors should have norm 1")
 
+	def get_values(self):
+		return np.copy(self.__values)
+
+	def get_shape(self):
+		return self.__values.shape
+	
 	def norm(self):
 		return np.linalg.norm(self.__values, axis = 1)
 	
-	def log_map(self, x):
+	def mobius_add(self, x):
+		''' Performs x + y. Performed for each vector y in self array. Returns an array of vectors.'''
+		scalar_prod = np.dot(x.get_values(), self.__values.transpose()).ravel()
+		norm_self = self.norm()
+		norm_x = x.norm()
+#		print(np.outer(1+2*scalar_prod+norm_self, x.get_values().ravel()) + (1 - norm_x**2)*self.__values)
+#		print((1 + 2*scalar_prod + norm_x**2 + norm_self**2).reshape(-1,1))
+#		ret =  (np.outer(1+2*scalar_prod+norm_self, x.get_values().ravel()) + (1 - norm_x**2)*self.__values) / ((1 + 2*scalar_prod + norm_x**2 + norm_self**2).reshape(-1,1))
+#		print(ret)
+		return (np.outer(1+2*scalar_prod+norm_self, x.get_values().ravel()) + (1 - norm_x**2)*self.__values) / ((1 + 2*scalar_prod + norm_x**2 + norm_self**2).reshape(-1,1))
+		#query: <x,y> means scalar prod
+	
 
+	def log_map(self, x):
 		if x.get_shape()[1] != self.__values.shape[1]:
 			raise Exception("Dimensions of inputs not matching: %d and %d"%(x.shape[1], self.__values.shape[1]))
 		if x.get_shape()[0] != 1:
@@ -45,31 +63,25 @@ class Points():
 		self.__ref__vec = None
 		self.__curv = 0
 
-	def get_values(self):
-		return np.copy(self.__values)
 
-	def get_shape(self):
-		return self.__values.shape
-
-	def mobius_add(self, x):
-		''' Performs x + y. Performed for each vector y in self array. Returns an array of vectors.'''
-		scalar_prod = np.dot(x.get_values(), self.__values.transpose()).ravel()
-		norm_self = self.norm()
-		norm_x = x.norm()
-#		print(np.outer(1+2*scalar_prod+norm_self, x.get_values().ravel()) + (1 - norm_x**2)*self.__values)
-#		print((1 + 2*scalar_prod + norm_x**2 + norm_self**2).reshape(-1,1))
-#		ret =  (np.outer(1+2*scalar_prod+norm_self, x.get_values().ravel()) + (1 - norm_x**2)*self.__values) / ((1 + 2*scalar_prod + norm_x**2 + norm_self**2).reshape(-1,1))
-#		print(ret)
-		return (np.outer(1+2*scalar_prod+norm_self, x.get_values().ravel()) + (1 - norm_x**2)*self.__values) / ((1 + 2*scalar_prod + norm_x**2 + norm_self**2).reshape(-1,1))
-		#query: <x,y> means scalar prod
-
+	def geodesic(self, x):
+		print(self.__values)
+		print(x.get_values())
+		if self.__curv == 0:
+			return np.linalg.norm(self.__values - x.get_values(), axis=1)
+		
+		elif self.__curv == -1:
+			print(np.linalg.norm(self.__values - x.get_values(), axis =1))
+			print((1-x.norm()**2)*(1-self.norm()**2))
+			print( np.arccosh( 1+2*(np.linalg.norm(self.__values - x.get_values(), axis =1)) / ((1-x.norm()**2)*(1-self.norm()**2))) )
 if __name__ == "__main__":
 	t1 = np.array([[-0.04, -0.03, -0.02],[-0.01,  0.00,  0.01],[ 0.02,  0.03,  0.04],[0.02, 0.04, 0.01]])
 	a = Points(t1)
 	t2 = np.array([[-0.04, -0.03, -0.02]])
 	b = Points(t2)
-	print(t1)
+	print(a.geodesic(b))
 	a.log_map(b)
 	print(a.get_values())
 	a.exp_map()
 	print(a.get_values())
+
