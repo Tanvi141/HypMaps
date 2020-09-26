1c6278c2637ec71b233a97335945929f27ad4b3a
Tanvi <tkarandikar@gmail.com>
2020-09-26 20:01:04 +0530

    mods for pointsless

--- src/trans.py
+++ src/trans.py
@@ -2,79 +2,45 @@ import numpy as np
 
 class Points():
 
-	def __init__(self, coordinates):
-		self.__curv = -1
-		self.__values = np.copy(coordinates) #a 2d array (dxN)
-		self.__ref_vec = None #a column vector
-		if np.all(self.norm() < 1) == False:
-			raise Exception("Vectors should have norm 1")
-
-	def get_values(self):
-		return np.copy(self.__values)
-
-	def get_shape(self):
-		return self.__values.shape
-	
-	def norm(self):
-		return np.linalg.norm(self.__values, axis = 1)
-	
-	def mobius_add(self, x):
+	def mobius_add(y, x):
 		''' Performs x + y. Performed for each vector y in self array. Returns an array of vectors.'''
-		scalar_prod = np.dot(x.get_values(), self.__values.transpose()).ravel()
-		norm_self = self.norm()
-		norm_x = x.norm()
+		scalar_prod = np.dot(x, y.T).ravel()
+		norm_self = np.linalg.norm(y, axis=1)
+		norm_x = np.linalg.norm(x, axis=1)
 #		print(np.outer(1+2*scalar_prod+norm_self, x.get_values().ravel()) + (1 - norm_x**2)*self.__values)
 #		print((1 + 2*scalar_prod + norm_x**2 + norm_self**2).reshape(-1,1))
 #		ret =  (np.outer(1+2*scalar_prod+norm_self, x.get_values().ravel()) + (1 - norm_x**2)*self.__values) / ((1 + 2*scalar_prod + norm_x**2 + norm_self**2).reshape(-1,1))
 #		print(ret)
-		return (np.outer(1+2*scalar_prod+norm_self, x.get_values().ravel()) + (1 - norm_x**2)*self.__values) / ((1 + 2*scalar_prod + norm_x**2 + norm_self**2).reshape(-1,1))
-		#query: <x,y> means scalar prod
+		return (np.outer(1+2*scalar_prod+norm_self, x.ravel()) + (1 - norm_x**2)*y) / ((1 + 2*scalar_prod + norm_x**2 + norm_self**2).reshape(-1,1))
 	
 
-	def log_map(self, x):
-		if x.get_shape()[1] != self.__values.shape[1]:
+	def log_map(y, x):
+		if x.shape[1] != y.shape[1]:
 			raise Exception("Dimensions of inputs not matching: %d and %d"%(x.shape[1], self.__values.shape[1]))
-		if x.get_shape()[0] != 1:
+		if x.shape[0] != 1:
 			raise Exception("Input should be a single vector")
 		
-		if self.__curv != -1:
-			raise Exception("Vectors already in euclidean space") 	
-	
-		self.__ref_vec = Points(x.get_values())
-		self.__curv = 0
-
-		mob_sum = self.mobius_add(x)
+		norm_x = np.linalg.norm(x, axis=1)
+		mob_sum = mobius_add(y, x)
 		mob_norm = np.linalg.norm(mob_sum, axis=1)
-		self.__values = mob_sum * ((1-x.norm()**2) * (np.arctanh(mob_norm) / mob_norm))[:, np.newaxis]
+		return mob_sum * ((1-norm_x**2) * (np.arctanh(mob_norm) / mob_norm))[:, np.newaxis]
 	
-	def exp_map(self):
-	#take in x an input
-		if self.__curv != 0:
-			raise Exception("Vectors already in hyp space") 	
-	
-		#self.__ref_vec.log_map(self.__ref_vec)
-		x= self.__ref_vec
-#		print(np.tanh(self.norm()/(1-x.norm()**2)))
-#		print(self.__values/self.norm()[:, np.newaxis])
-#		print((self.__values/self.norm()[:, np.newaxis])*(np.tanh(self.norm()/(1-x.norm()**2)))[:, np.newaxis])
-		temp_arr = Points((self.__values/self.norm()[:, np.newaxis])*(np.tanh(self.norm()/(1-x.norm()**2)))[:, np.newaxis])
-		self.__values = temp_arr.mobius_add(x)	
-		
-		self.__ref__vec = None
-		self.__curv = -1
 
-
-	def geodesic(self, x):
-#print(self.__values)
-#		print(x.get_values())
-		if self.__curv == 0:
-			return np.linalg.norm(self.__values - x.get_values(), axis=1)
+	def exp_map(y, x):
+		if x.shape[1] != y.shape[1]:
+			raise Exception("Dimensions of inputs not matching: %d and %d"%(x.shape[1], self.__values.shape[1]))
+		if x.shape[0] != 1:
+			raise Exception("Input should be a single vector")
 		
-		elif self.__curv == -1:
-#			print(np.linalg.norm(self.__values - x.get_values(), axis =1))
-#			print((1-x.norm()**2)*(1-self.norm()**2))
-#			print( np.arccosh( 1+2*(np.linalg.norm(self.__values - x.get_values(), axis =1)) / ((1-x.norm()**2)*(1-self.norm()**2))) )
-			pass
+		norm_y = np.linalg.norm(y, axis=1)
+		temp_arr = (y/norm_y[:, np.newaxis])*(np.tanh(norm_y()/(1-norm_x()**2)))[:, np.newaxis]
+		return mobius_add(temp_arr, x)	
+		
+
+	def geodesic(y, x):
+		norm_x = np.linalg.norm(x, axis=1)
+		norm_y = np.linalg.norm(y, axis=1)
+		return np.arccosh( 1+2*(np.linalg.norm(y - x, axis =1)) / ((1-norm_x**2)*(1-norm_y**2))) 
 
 if __name__ == "__main__":
 	t1 = np.array([[-0.04, -0.03, -0.02],[-0.01,  0.00,  0.01],[ 0.02,  0.03,  0.04],[0.02, 0.04, 0.01]])
