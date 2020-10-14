7aa86e3e63563a78fce2a35575c46fca1a47abd2
Tanvi <tkarandikar@gmail.com>
2020-10-14 17:13:17 +0530

    final version for upload

--- hypmaps/poincare.py
+++ hypmaps/poincare.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class Poincare():
+class Transform():
 	
 	def __init__(self, EPS):
 		self.EPS = EPS
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="hypmapss", 
+    name="hypmaps", 
     version="0.0.1",
     author="Tanvi Karandikar",
     author_email="tanvi.karandikar141@gmail.com",
