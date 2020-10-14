ca3071c0fe86ec2bfe45ac9c075e12dba123d75d
Tanvi <tkarandikar@gmail.com>
2020-10-14 11:43:49 +0530

    better precision

--- src/trans.py
+++ src/trans.py
@@ -4,22 +4,26 @@ class Points():
 	
 	def __init__(self, EPS):
 		self.EPS = EPS
+		print(self.EPS)
 
 	def th_atanh(self, x):
 		#values = np.min(x, [1.0 - EPS])
 		return 0.5 * (np.log(1 + x) - np.log(1 - x))
 
 	def lambda_x(self, x):
-		return 2. / (1 - np.dot(x, x.T))
+		return 2. / (1 - np.einsum("ij,ij->i", x, x))
 	
-	def mobius_add(self, y, x):
-		''' Performs x + y. Performed for each vector y in self array. Returns an array of vectors.'''
-		scalar_prod = np.dot(x, y.T).ravel()
-		print(scalar_prod)
-		norm_self = np.dot(y, y.T) 
-		norm_x = np.dot(x, x.T)
-		print(norm_self, norm_x)
-		return (np.outer(1+2*scalar_prod+norm_self, x.ravel()) + (1 - norm_x**2)*y) / ((1 + 2*scalar_prod + norm_x**2 + norm_self**2).reshape(-1,1))
+	def dot(self, u, v):
+		return np.einsum("ij,ij->i", u, v) 
+
+	def mobius_add(self, u, v):
+		v = v + self.EPS
+		dot_u_v = 2. * self.dot(u, v)
+		norm_u_sq = self.dot(u, u)
+		norm_v_sq = self.dot(v, v)
+		denominator = 1. + dot_u_v + norm_v_sq * norm_u_sq
+		return (1. + dot_u_v + norm_v_sq) / (denominator + self.EPS) * u + \
+         	     (1. - norm_u_sq) / (denominator + self.EPS) * v
 	
 
 	def log_map(self, x, y):
@@ -28,8 +32,8 @@ class Points():
 		if x.shape[0] != 1:
 			raise Exception("Input should be a single vector")
 		
-		diff = self.mobius_add(-x, y) 
-		norm_diff = np.dot(diff, diff.T)
+		diff = self.mobius_add(-x, y) + self.EPS 
+		norm_diff = self.dot(diff, diff.T)
 		lam = self.lambda_x(x)
 		return (( 2. / lam) * self.th_atanh(norm_diff) / norm_diff) * diff
 	
@@ -40,22 +44,31 @@ class Points():
 		if x.shape[0] != 1:
 			raise Exception("Input should be a single vector")
 		
-		norm_v = np.dot(v, v.T)
+		v = v + self.EPS
+		norm_v = self.dot(v, v)
 		second_term = (np.tanh(self.lambda_x(x) * norm_v / 2) / norm_v) * v
 		return (self.mobius_add(x, second_term))
 
 
 	def geodesic(self, y, x):
-		norm_x = np.linalg.norm(x, axis=1)
-		norm_y = np.linalg.norm(y, axis=1)
-		return np.arccosh( 1+2*(np.linalg.norm(y - x, axis =1)) / ((1-norm_x**2)*(1-norm_y**2))) 
+		norm_x = np.dot(x, x)
+		norm_y = np.dot(y, y)
+		return np.arccosh( 1+2*(self.dot(y - x, (y-x).T)) / ((1-norm_x**2)*(1-norm_y**2))) 
 
 if __name__ == "__main__":
-	a = Points(0.03)
-	x = np.array([[0.02, 0.04, 0.01]])
-	y = np.array([[-0.04, -0.03, -0.02]])
+	a = Points(0)
+	y = np.array([[0.02, 0.04, 0.01]])
+	x = np.array([[-0.04, -0.03, -0.02]])
+	print("x and y", x, y)
+	#print("mob_add", a.mobius_add(y, x))
 	log_x = a.log_map(x, y)
-	print(log_x)
-	print(a.exp_map(x, log_x))
+	print("log map", log_x)
+	print("exp map", a.exp_map(x, log_x))
 	#print(geodesic())	
-
+	print("\n\non array")	
+	y2 = np.array([[0.02, 0.04, 0.01], [0.02, 0.04, 0.01]])
+	#print("mob_add", a.mobius_add(y2, x))
+	log_x = a.log_map(x, y2)
+	print("log map", log_x)
+	#print("exp map", a.exp_map_try(x, log_x))
+	
